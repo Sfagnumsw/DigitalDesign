2e09df258f9acc94e02b961001b7fce980edd022
Sfagnumsw <49709456+Sfagnumsw@users.noreply.github.com>
2021-03-12 17:47:25 +0300

    Update DLL.cs

--- Отражение/DLL.cs
+++ Отражение/DLL.cs
@@ -6,26 +6,27 @@ using System.Linq;
 namespace MyLibrary
 {
     public class Class1
-    {
+    {       
         public Dictionary<string, int> Words = new Dictionary<string, int>();
         private Dictionary<string, int> Catcher(String text)
         {
-            Regex regex1 = new Regex(@"человек(\w*)");
-            MatchCollection matches1 = regex1.Matches(text);
-            Words.Add("человек", matches1.Count);
+            Regex regex = new Regex("[^A-Za-zА-Яа-я]");
+            text = regex.Replace(text, " ");
+            text = text.ToLower();
 
-            Regex regex2 = new Regex(@"владык(\w*)");
-            MatchCollection matches2 = regex2.Matches(text);
-            Words.Add("владыка", matches2.Count);
-
-            Regex regex3 = new Regex(@"увидел(\w*)");
-            MatchCollection matches3 = regex3.Matches(text);
-            Words.Add("увидел", matches3.Count);
-
-            Regex regex4 = new Regex(@"красив(\w*)");
-            MatchCollection matches4 = regex4.Matches(text);
-            Words.Add("красиво", matches4.Count);
-
+            string[] MyList = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(string word in MyList)
+            {
+                if (!Words.ContainsKey(word)) 
+                {
+                    Words.Add(word, 1);
+                }
+                else
+                {
+                    Words.TryGetValue(word, out int value);
+                    Words[word] = value + 1;
+                }
+            }
             Words = Words.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
             return Words;
         }
